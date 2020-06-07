e554f97ef2f2d9d56c4e5433eb2b3d43fd285b9c
surpace <54321436+surpace@users.noreply.github.com>
2020-06-07 17:46:41 +0900

    Update solve-linear-equations.rb
    複素数・有理数に対応、出力を読みやすくした

--- solve-linear-equations.rb
+++ solve-linear-equations.rb
@@ -1,5 +1,32 @@
 require "matrix"
 
+#----BEGIN FUNCTIONS----
+# ---- util section ----
+
+def pretty_complex(z)
+	real, imag = z.real, z.imag
+	if real.abs < Float::EPSILON
+		return pretty_real(imag) + "i"
+	elsif imag.abs < Float::EPSILON
+		return pretty_real(real)
+	else
+		return z.inspect
+	end
+end
+
+def pretty_real(x)
+	return "0" if x.abs < Float::EPSILON
+
+	if Rational === x
+		return pretty_real(x.numerator) if 1 === x.denominator
+	end
+
+	return x.inspect
+end
+
+#----END FUNCTIONS----
+
+
 print "Number of variables: "
 n = gets.to_i
 
@@ -9,11 +36,12 @@ coeffs = []
 consts = []
 n.times do |i|
 	print "Equation (#{i+1}): "
-	*coeff, const = gets.split.map(&:to_f)
+	*coeff, const = gets.split.map(&:to_c)
 	coeffs << coeff
 	consts << const
 end
 
 puts "Solving..."
 
-p Matrix[*coeffs].lup.solve(consts)
+solution = Matrix[*coeffs].lup.solve(consts)
+puts "Solution is [" + solution.to_a.map { |z| pretty_complex(z) }.join(", ") + "]"
