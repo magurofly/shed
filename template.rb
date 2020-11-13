c700cf93a13aa9cbde01c62980014b3fea0afdba
magurofly <54321436+magurofly@users.noreply.github.com>
2020-11-13 12:08:39 +0900

    Update template.rb

--- template.rb
+++ template.rb
@@ -20,6 +20,7 @@ def minmax(*xs); xs.minmax; end
 def matrix(h, w, fill=nil); Array.new(h) { [fill] * w }; end
 def debug(x); STDERR.puts x.inspect if DEBUG; end
 
+def div_ceil(x, y); (x + y - 1) / y; end
 def gcd(*xs); xs.inject(0) { |y, x| y.gcd(x) }; end
 def factorial(n, mod); (2..n).inject(1) { |f, x| f * x % mod }; end
 def cumsum(xs); ys = [0]; xs.each { |x| ys << x + ys[-1] }; ys; end
