aea38610b9de69bd201579cecdcb07accbb875d0
magurofly <54321436+magurofly@users.noreply.github.com>
2021-01-18 21:53:49 +0900

    Update primes.rb

--- primes.rb
+++ primes.rb
@@ -10,3 +10,22 @@ class SPFPrime
 	def each; (2..max).each { |i| yield i if @spf[i] == i }; end
 	def factorize(n); fs = []; while n > 1; f = @spf[n]; fs << f; n /= f; end; fs; end
 end
+
+# O(log n) で素数判定をする ただし n＜2^64
+def prime?(n)
+  return false if n <= 1
+  return true if n == 2 or n == 7 or n == 61
+  return false if (n & 1) == 0
+  d = n - 1
+  d >>= 1 while (d & 1) == 0
+  [2, 7, 61].each do |a|
+    t = d
+    y = a.pow(t, n)
+    while t != n - 1 and y != 1 and y != n - 1
+      y = y * y % n
+      t <<= 1
+    end
+    return false if y != n - 1 and (t & 1) == 0
+  end
+  true
+end
