5c9c0d4b416f9057e02b9b85acaaaff89568a8ed
magurofly <54321436+magurofly@users.noreply.github.com>
2021-02-16 19:38:21 +0900

    Update template.rs

--- template.rs
+++ template.rs
@@ -7,12 +7,19 @@ fn main() {
   
 }
 
+type Int = i64;
+const MOD: Int = 1_000_000_007;
+const INF: Int = 1_000_000_000;
 const YESNO: [&'static str; 2] = ["Yes", "No"];
 
 use proconio::{input, fastout, marker::{Chars, Bytes, Usize1}};
-use std::collections::{VecDeque, HashMap, HashSet, BTreeMap, BTreeSet, BinaryHeap};
-use std::cmp::{self, Reverse}; // cmp::max, cmp::min
+use std::*;
+use collections::*; // (BTree|Hash)(Set|Map), BinaryHeap, VecDeque, LinkedList
+use cmp::{self, Reverse}; // cmp::{min, max}
 use itertools::Itertools as _;
+use num_traits::*;
+use petgraph::prelude::*; // (Stable)?(Di|Un)?Graph, (Di|Un)?GraphMap, (Node|Edge)Index, Bfs, Dfs, DfsPostOrder
+use petgraph::unionfind::UnionFind;
 
 fn yes() { println!("{}", YESNO[0]); }
 fn no() { println!("{}", YESNO[1]); }
