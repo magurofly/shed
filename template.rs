a219b5b5b079309d252dafece3c675a621ff1adc
magurofly <54321436+magurofly@users.noreply.github.com>
2021-02-18 01:24:10 +0900

    Update template.rs

--- template.rs
+++ template.rs
@@ -1,4 +1,4 @@
-#![allow(dead_code, unused_imports)]
+#![allow(dead_code, unused_imports, unused_macros)]
 
 #[fastout]
 fn main() {
@@ -7,7 +7,7 @@ fn main() {
   
 }
 
-type Int = i64;
+type Int = usize;
 const MOD: Int = 1_000_000_007;
 const INF: Int = 1_000_000_000;
 const YESNO: [&'static str; 2] = ["Yes", "No"];
@@ -24,3 +24,46 @@ use petgraph::unionfind::UnionFind;
 fn yes() { println!("{}", YESNO[0]); }
 fn no() { println!("{}", YESNO[1]); }
 fn yesno(c: bool) { println!("{}", if c { YESNO[0] } else { YESNO[1] }); }
+macro_rules! min {
+  ($a:expr $(,)*) => {{ $a }};
+  ($a:expr, $b:expr $(,)*) => {{ cmp::min($a, $b) }};
+  ($a:expr, $($rest:expr),+ $(,)*) => {{ cmp::min($a, min!($($rest),+)) }}
+}
+macro_rules! max {
+  ($a:expr $(,)*) => {{ $a }};
+  ($a:expr, $b:expr $(,)*) => {{ cmp::max($a, $b) }};
+  ($a:expr, $($rest:expr),+ $(,)*) => {{ cmp::max($a, max!($($rest),+)) }}
+}
+
+trait MyItertools : Iterator {
+  fn tally(self) -> HashMap<Self::Item, usize> where Self: Sized, Self::Item: Copy + Eq + hash::Hash {
+    let mut counts = HashMap::new();
+    self.for_each(|item| *counts.entry(item).or_default() += 1 );
+    counts
+  }
+  fn cumprod<F: Fn(Self::Item, Self::Item) -> Self::Item>(self, init: Self::Item, f: F) -> CumProd<Self, Self::Item, F> where Self: Sized, Self::Item: Copy { CumProd { prod: init, f, iter: self } }
+}
+impl<T: ?Sized> MyItertools for T where T: Iterator {}
+
+struct CumProd<I, P, F> { prod: P, iter: I, f: F }
+impl<I: Iterator<Item = P>, P: Copy, F: Fn(P, P) -> P> Iterator for CumProd<I, P, F> {
+  type Item = P;
+  fn next(&mut self) -> Option<Self::Item> {
+    let mut z = self.prod;
+    self.iter.next().map(|x| { z = (self.f)(z, x); z })
+  }
+  fn size_hint(&self) -> (usize, Option<usize>) { self.iter.size_hint() }
+}
+impl<I: Iterator<Item = P>, P: Copy, F: Fn(P, P) -> P> ExactSizeIterator for CumProd<I, P, F> {}
+
+trait MyOrd : PartialOrd {
+  fn chmax(&mut self, mut rhs: Self) -> &mut Self where Self: Sized {
+    if self < &mut rhs { *self = rhs; }
+    self
+  }
+  fn chmin(&mut self, mut rhs: Self) -> &mut Self where Self: Sized {
+    if self > &mut rhs { *self = rhs; }
+    self
+  }
+}
+impl<T: ?Sized> MyOrd for T where T: PartialOrd {}
