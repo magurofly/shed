1139e81bdfe76ba1a3aec4f33f6f4d344c2e14c5
magurofly <54321436+magurofly@users.noreply.github.com>
2021-02-18 20:17:08 +0900

    Update template.rs

--- template.rs
+++ template.rs
@@ -3,8 +3,31 @@
 #[fastout]
 fn main() {
   input! {
+    n: usize, m: usize,
+    s: Chars,
   }
   
+  let indices = s.into_iter().enumerate()
+    .map(|(i, c)| if c == '0' { i } else { INF })
+    .rev()
+    .cumprod(INF, cmp::min)
+    .to_vec_reversed();
+
+  // eprintln!("{:?}", &indices);
+
+  let mut index = n;
+  let mut ans = vec![];
+  while index > 0 {
+    let next = if index < m { indices[0] } else { indices[index - m] };
+    if next >= index {
+      println!("-1");
+      return;
+    }
+    ans.push(index - next);
+    index = next;
+  }
+
+  println!("{}", ans.iter().rev().join(" "));
 }
 
 type Int = usize;
@@ -36,6 +59,8 @@ macro_rules! max {
 }
 
 trait MyItertools : Iterator {
+  fn to_vec(self) -> Vec<Self::Item> where Self: Sized { self.collect::<Vec<_>>() }
+  fn to_vec_reversed(self) -> Vec<Self::Item> where Self: Sized { let mut v = self.collect::<Vec<_>>(); v.reverse(); v }
   fn tally(self) -> HashMap<Self::Item, usize> where Self: Sized, Self::Item: Copy + Eq + hash::Hash {
     let mut counts = HashMap::new();
     self.for_each(|item| *counts.entry(item).or_default() += 1 );
@@ -48,22 +73,13 @@ impl<T: ?Sized> MyItertools for T where T: Iterator {}
 struct CumProd<I, P, F> { prod: P, iter: I, f: F }
 impl<I: Iterator<Item = P>, P: Copy, F: Fn(P, P) -> P> Iterator for CumProd<I, P, F> {
   type Item = P;
-  fn next(&mut self) -> Option<Self::Item> {
-    let mut z = self.prod;
-    self.iter.next().map(|x| { z = (self.f)(z, x); z })
-  }
+  fn next(&mut self) -> Option<Self::Item> { self.iter.next().map(|x| { self.prod = (self.f)(self.prod.clone(), x); self.prod.clone() }) }
   fn size_hint(&self) -> (usize, Option<usize>) { self.iter.size_hint() }
 }
 impl<I: Iterator<Item = P>, P: Copy, F: Fn(P, P) -> P> ExactSizeIterator for CumProd<I, P, F> {}
 
 trait MyOrd : PartialOrd {
-  fn chmax(&mut self, mut rhs: Self) -> &mut Self where Self: Sized {
-    if self < &mut rhs { *self = rhs; }
-    self
-  }
-  fn chmin(&mut self, mut rhs: Self) -> &mut Self where Self: Sized {
-    if self > &mut rhs { *self = rhs; }
-    self
-  }
+  fn chmax(&mut self, mut rhs: Self) -> &mut Self where Self: Sized { if self < &mut rhs { *self = rhs; }; self }
+  fn chmin(&mut self, mut rhs: Self) -> &mut Self where Self: Sized { if self > &mut rhs { *self = rhs; }; self }
 }
 impl<T: ?Sized> MyOrd for T where T: PartialOrd {}
