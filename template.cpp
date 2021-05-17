3ae7d69b0dfebeb06e391d7a13741f2e3d8e745f
magurofly <54321436+magurofly@users.noreply.github.com>
2021-05-17 18:27:29 +0900

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define rep(i, l, r) for (auto i = (l); i < (r); i++)
+#define chmin(dest, src) if ((dest) > (src)) dest = (src)
+#define chmax(dest, src) if ((dest) < (src)) dest = (src)
+using ll = int;
+
+int main() {
+
+  return 0;
+}
