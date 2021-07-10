9d8b67d9f9b7022d0f8327bc481ae933f4d56e96
magurofly <54321436+magurofly@users.noreply.github.com>
2021-07-10 18:10:31 +0900

    Update radix-sort.rs

--- radix-sort.rs
+++ radix-sort.rs
@@ -1,16 +1,16 @@
 fn main() {
   let mut xs = vec![(3, 0), (2, 1), (6, 2), (1, 3), (3, 4)];
   println!("{:?}", &xs);
-  radix_sort_u32_by(&mut xs, |x| x.0 );
+  radix_sort_by(&mut xs, 2, |x| x.0 );
   println!("{:?}", &xs);
 }
 
-fn radix_sort_u32_by<T>(xs: &mut Vec<T>, mut f: impl FnMut(&T) -> u32) {
+fn radix_sort_by<T>(xs: &mut Vec<T>, bits: usize, mut f: impl FnMut(&T) -> usize) {
   let mut buckets = Vec::new();
   buckets.resize_with(16, Vec::new);
-  for d in 0 .. 8 {
+  for d in 0 .. (bits + 3) / 4 {
     for x in xs.drain(..) {
-      let i = ((f)(&x) >> 4 * d & 15) as usize;
+      let i = (f)(&x) >> 4 * d & 15;
       buckets[i].push(x);
     }
     for bucket in &mut buckets {
