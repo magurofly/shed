a356f35fd0943fe420540ffc44fe7be4adf38545
magurofly <54321436+magurofly@users.noreply.github.com>
2021-07-24 16:38:31 +0900

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -3,9 +3,14 @@
 using namespace std;
 using namespace atcoder;
 #define rep(i, l, r) for (auto i = (l); i < (r); i++)
+#define all(a) a.begin(), a.end()
 #define chmin(dest, src) if ((dest) > (src)) dest = (src)
 #define chmax(dest, src) if ((dest) < (src)) dest = (src)
 using ll = int;
+using i64 = long long;
+using i32 = long;
+using u64 = unsigned long long;
+using u32 = unsigned long;
 
 int main() {
 
