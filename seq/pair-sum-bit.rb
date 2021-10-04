bda68ab937f236f8bfc81837ab3ee92aa7818cb0
magurofly <54321436+magurofly@users.noreply.github.com>
2021-10-04 11:48:30 +0900

    Create pair-sum-bit.rb

---
+++ seq/pair-sum-bit.rb
@@ -0,0 +1,68 @@
+# xs の非順序対のANDの総和を計算する
+# 非負整数のみ
+# ```
+# pair_sum_and(xs) == xs.combination(2).sum { _1 & _2 }
+# ```
+def pair_sum_and(xs)
+  n = xs.size
+  m = xs.map(&:bit_length).max
+  (0 ... m).sum do |bit|
+    ans = 0
+    sum = 0
+    (n - 2).downto(0) do |i|
+      sum += xs[i + 1][bit]
+      ans += xs[i][bit] * sum
+    end
+    ans << bit
+  end
+end
+
+# xs の非順序対のORの総和を計算する
+# 非負整数のみ
+# ```
+# pair_sum_or(xs) == xs.combination(2).sum { _1 | _2 }
+# ```
+def pair_sum_or(xs)
+  n = xs.size
+  m = xs.map(&:bit_length).max
+  (0 ... m).sum do |bit|
+    ans = 0
+    sum = 0
+    (n - 2).downto(0) do |i|
+      sum += xs[i + 1][bit]
+      ans +=
+        case xs[i][bit]
+        when 0
+          sum
+        when 1
+          n - i - 1
+        end
+    end
+    ans << bit
+  end
+end
+
+# xs の非順序対のXORの総和を計算する
+# 非負整数のみ
+# ```
+# pair_sum_xor(xs) == xs.combination(2).sum { _1 ^ _2 }
+# ```
+def pair_sum_xor(xs)
+  n = xs.size
+  m = xs.map(&:bit_length).max
+  (0 ... m).sum do |bit|
+    ans = 0
+    count = [0, 0]
+    (n - 2).downto(0) do |i|
+      count[xs[i + 1][bit]] += 1
+      ans +=
+        case xs[i][bit]
+        when 0
+          count[1]
+        when 1
+          count[0]
+        end
+    end
+    ans << bit
+  end
+end
