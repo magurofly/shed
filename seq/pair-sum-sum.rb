7f2c62c9d85022d00c2170592ecd6684c56cb3bc
magurofly <54321436+magurofly@users.noreply.github.com>
2021-10-04 12:35:35 +0900

    Update pair-sum-sum.rb

--- seq/pair-sum-sum.rb
+++ seq/pair-sum-sum.rb
@@ -1,14 +1,15 @@
 # xs の非順序対の和の総和を計算する
 # ```
 # pair_sum_sum(xs) == xs.combination(2).sum { _1 + _2 }
+# pair_sum_sum(xs, a, b, c) == xs.combination(2).sum { a * _1 + b * _2 + c }
 # ```
-def pair_sum_sum(xs)
+def pair_sum_sum(xs, a = 1, b = 1, c = 0)
   n = xs.size
   ans = 0
   sum = 0
-  (n - 2).downto(0) do |i|
-    sum += xs[i + 1]
-    ans += (n - i - 1) * xs[i] + sum
+  (n - 1).downto(0) do |i|
+    ans += (n - i - 1) * (a * xs[i] + c) + b * sum
+    sum += xs[i]
   end
   ans
 end
