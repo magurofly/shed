07dbdda9b251dc349e32f2b7da5d0c6d4a23d079
magurofly <54321436+magurofly@users.noreply.github.com>
2022-01-25 12:17:38 +0900

    Update template.rs

--- template.rs
+++ template.rs
@@ -9,8 +9,9 @@ fn main() {
 }
 
 type Int = i64;
-type Mint = ModInt<Int>;
-const MOD: Int = 1_000_000_007;
+type Mod = Mod1000000007;
+// type Mod = Mod998244353;
+const MOD: Int = Mod::modulus();
 const INF: Int = 1_000_000_000;
 const YESNO: [&'static str; 2] = ["Yes", "No"];
 
@@ -25,6 +26,7 @@ use num_integer::*;
 use petgraph::prelude::*; // (Stable)?(Di|Un)?Graph, (Di|Un)?GraphMap, (Node|Edge)Index, Bfs, Dfs, DfsPostOrder
 use petgraph::unionfind::UnionFind;
 
+type Mint = ModInt<Mod>; fn mint(x: impl ToPrimitive) -> Mint { Mint::new(x.to_i64()); }
 fn yes() { println!("{}", YESNO[0]); }
 fn no() { println!("{}", YESNO[1]); }
 fn yesno(c: bool) { println!("{}", if c { YESNO[0] } else { YESNO[1] }); }
@@ -58,8 +60,6 @@ impl<I: Iterator<Item = P>, P: Copy, F: Fn(P, P) -> P> Iterator for CumProd<I, P
 impl<I: Iterator<Item = P>, P: Copy, F: Fn(P, P) -> P> ExactSizeIterator for CumProd<I, P, F> {}
 
 trait MyOrd : PartialOrd + Sized {
-  fn max(self, other: Self) -> Self { if &self < &other { other } else { self } }
-  fn min(self, other: Self) -> Self { if &self > &other { other } else { self } }
   fn chmax(&mut self, mut rhs: Self) -> &mut Self { if self < &mut rhs { *self = rhs; }; self }
   fn chmin(&mut self, mut rhs: Self) -> &mut Self { if self > &mut rhs { *self = rhs; }; self }
 }
@@ -73,23 +73,35 @@ trait MyPrimInt : PrimInt {
 }
 impl<T: PrimInt> MyPrimInt for T {}
 
-#[derive(Clone, Copy)]
-struct ModInt<C>(C, C);
-impl<C: PrimInt> ModInt<C> {
-  fn new(value: C, modulo: C) -> Self { Self(value, modulo) }
-  fn to_int(&self) -> C { (self.0 + self.1) % self.1 }
-  fn pow<E: PrimInt>(self, mut exp: E) -> Self { let mut r = C::one(); let mut b = self.0; while !exp.is_zero() { if (exp & E::one()).is_one() { r = r * b % self.1; } b = b * b % self.1; exp = exp >> 1; } Self(r, self.1) }
-  fn inv(self) -> Self { self.pow(self.1 - C::one() - C::one()) }
+pub trait Modulus { fn modulus() -> i64; }
+pub struct Mod1000000007; impl Modulus for Mod1000000007 { fn modulus() -> i64 { 1000000007 } }
+pub struct Mod998244353; impl Modulus for Mod998244353 { fn modulus() -> i64 { 998244353 } }
+
+pub struct ModInt<M>(i64, std::marker::PhantomData<M>);
+impl<M> ModInt<M> {
+  pub fn new(n: i64) -> Self where M: Modulus { Self::raw(n.rem_euclid(M::modulus())) }
+  pub fn raw(n: i64) -> Self { Self(n, std::marker::PhantomData) }
+  pub fn val(&self) -> i64 { self.0 }
 }
-impl<C: PrimInt + From<Int>> From<C> for ModInt<C> { fn from(value: C) -> Self { Self(value, From::from(MOD)) } }
-impl<C: PrimInt + fmt::Debug> fmt::Debug for ModInt<C> { fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { write!(f, "({:?} mod {:?})", self.0, self.1) } }
-impl<C: PrimInt + fmt::Display> fmt::Display for ModInt<C> { fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { self.to_int().fmt(f) } }
-impl<C: PrimInt + From<Int>> Num for ModInt<C> { type FromStrRadixErr = C::FromStrRadixErr; fn from_str_radix(str: &str, radix: u32) -> Result<Self, Self::FromStrRadixErr> { Ok(Self::from(C::from_str_radix(str, radix)?)) } }
-impl<C: PrimInt + From<Int>> PartialEq for ModInt<C> { fn eq(&self, other: &Self) -> bool { self.0.eq(&other.0) } }
-impl<C: PrimInt + From<Int>> Zero for ModInt<C> { fn zero() -> Self { Self::from(C::zero()) } fn is_zero(&self) -> bool { self.0.is_zero() } }
-impl<C: PrimInt + From<Int>> One for ModInt<C> { fn one() -> Self { Self::from(C::one()) } }
-impl<C: PrimInt + From<Int>> Add for ModInt<C> { type Output = Self; fn add(self, rhs: Self) -> Self { Self((self.0 + rhs.0) % self.1, self.1) } }
-impl<C: PrimInt + From<Int>> Sub for ModInt<C> { type Output = Self; fn sub(self, rhs: Self) -> Self { Self((self.0 - rhs.0) % self.1, self.1) } }
-impl<C: PrimInt + From<Int>> Mul for ModInt<C> { type Output = Self; fn mul(self, rhs: Self) -> Self { Self(self.0 * rhs.0 % self.1, self.1) } }
-impl<C: PrimInt + From<Int>> Div for ModInt<C> { type Output = Self; fn div(self, rhs: Self) -> Self { self * rhs.inv() } }
-impl<C: PrimInt + From<Int>> Rem for ModInt<C> { type Output = Self; fn rem(self, rhs: Self) -> Self { Self(self.0 % rhs.0, self.1) } }
+impl<M> Clone for ModInt<M> { fn clone(&self) -> Self { Self::raw(self.0) } }
+impl<M> Copy for ModInt<M> {}
+impl<M> PartialEq for ModInt<M> { fn eq(&self, rhs: &Self) -> bool { self.0 == rhs.0 } }
+impl<M> Eq for ModInt<M> {}
+impl<M> fmt::Debug for ModInt<M> { fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { self.0.fmt(f) } }
+impl<M> fmt::Display for ModInt<M> { fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { self.0.fmt(f) } }
+impl<M: Modulus> str::FromStr for ModInt<M> { type Err = <i64 as str::FromStr>::Err; fn from_str(s: &str) -> Result<Self, Self::Err> { Ok(Self::new(i64::from_str(s)?)) } }
+impl<M> ToPrimitive for ModInt<M> { fn to_i64(&self) -> Option<i64> { Some(self.0) } fn to_u64(&self) -> Option<u64> { Some(self.0 as u64) } }
+impl<M: Modulus> Zero for ModInt<M> { fn zero() -> Self { Self::new(0) } fn is_zero(&self) -> bool { self.0 == 0 } }
+impl<M: Modulus> One for ModInt<M> { fn one() -> Self { Self::new(1) } fn is_one(&self) -> bool { self.0 == 1 } }
+macro_rules! impl_op { ($M:ident, $T:ident $f:ident, $U:ident $g:ident, |$x:ident, $y:ident| $b:block) => {
+  impl <$M: Modulus, A: ToPrimitive> $T<A> for ModInt<$M> { type Output = Self; fn $f(self, y: A) -> Self { let ($x, $y) = (self.0, y.to_i64().unwrap()); $b } }
+  impl <$M: Modulus, A: ToPrimitive> $U<A> for ModInt<$M> { fn $g(&mut self, y: A) { *self = self.$f(y); } }
+} }
+impl_op!(M, Add add, AddAssign add_assign, |x, y| { Self::new(x + y) });
+impl_op!(M, Sub sub, SubAssign sub_assign, |x, y| { Self::new(x - y) });
+impl_op!(M, Mul mul, MulAssign mul_assign, |x, y| { Self::raw((x as i128 * y as i128).rem_euclid(M::modulus() as i128) as i64) });
+impl_op!(M, Div div, DivAssign div_assign, |x, y| { Self::raw(x) * Self::new(y).inv() });
+impl_op!(M, Rem rem, RemAssign rem_assign, |x, y| { Self::new(x % y) });
+impl<M: Modulus> Num for ModInt<M> { type FromStrRadixErr = <i64 as Num>::FromStrRadixErr; fn from_str_radix(str: &str, radix: u32) -> Result<Self, Self::FromStrRadixErr> { Ok(Self::new(i64::from_str_radix(str, radix)?)) } }
+impl<M: Modulus, A: PrimInt> Pow<A> for ModInt<M> { type Output = Self; fn pow(self, mut e: A) -> Self { let mut r = Self::one(); let mut x = self; while !e.is_zero() { if (e & A::one()).is_one() { r = r * x; } x = x * x; e = e >> 1; } r } }
+impl<M: Modulus> Inv for ModInt<M> { type Output = Self; fn inv(self) -> Self { let (mut s, mut t) = ((self.0, 0), (self.0.rem_euclid(M::modulus()), 1)); while t.0 != 0 { let u = s.0 / t.0; s = (s.0 - t.0 * u, s.1 - t.1 * u); std::mem::swap(&mut s, &mut t); } if s.0 < 0 { s.0 = s.0 + M::modulus() / s.0; } Self::raw(s.1) } }
