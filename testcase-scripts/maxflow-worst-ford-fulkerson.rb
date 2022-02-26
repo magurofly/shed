cc43a9eded3e02fd1b358be11f649cf886626ca7
magurofly <54321436+magurofly@users.noreply.github.com>
2022-02-26 10:24:54 +0900

    Create maxflow-worst-ford-fulkerson.rb

---
+++ testcase-scripts/maxflow-worst-ford-fulkerson.rb
@@ -0,0 +1,31 @@
+index_start = 1 # 0 にすると 0-index
+cap_max = 10**9
+
+STDERR.print "level = "
+level = gets.to_i
+raise "level must be >= 1" if level < 1
+
+N = 2 * level
+STDERR.puts "N = #{N}"
+
+arcs = []
+
+# level 1
+s = N / 2 - 1
+t = N / 2
+arcs << [s, t, 1]
+
+(level - 1).times do |i|
+  a, b = s - 1, t + 1
+  cap = [cap_max, 2**i].min
+  arcs << [a, s, cap] << [a, t, cap] << [s, b, cap] << [t, b, cap]
+  s, t = a, b
+end
+
+STDERR.puts "s = #{s}"
+STDERR.puts "t = #{t}"
+
+puts "#{N} #{arcs.size}"
+arcs.each do |(u, v, c)|
+  puts "#{u + index_start} #{v + index_start} #{c}"
+end
