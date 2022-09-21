4a91f2f983243fac88dbca1ec7775f6ed0055dd0
magurofly <54321436+magurofly@users.noreply.github.com>
2022-09-22 08:10:06 +0900

    Update swag-deque.rs

--- swag-deque.rs
+++ swag-deque.rs
@@ -13,7 +13,7 @@ impl<T> SumDeque<T> where T: Clone + std::ops::Add<Output = T> {
     pub fn pop_front(&mut self) -> Option<T> { if self.front.is_empty() { self.front = Self::split(&mut self.back); } Some(self.front.pop()?.0) }
     pub fn sum(&self) -> Option<T> { Self::merge(Self::second(self.front.last()), Self::second(self.back.last())) }
     fn refresh(a: &mut [(T, T)]) { let mut y = None; for (x, z) in a { y = Self::merge(y, Some(x.clone())); *z = y.clone().unwrap(); } }
-    fn split(a: &mut Vec<(T, T)>) -> Vec<(T, T)> { let mut b = a.drain(.. a.len() / 2).collect::<Vec<_>>(); b.reverse(); Self::refresh(a); Self::refresh(&mut b); b }
+    fn split(a: &mut Vec<(T, T)>) -> Vec<(T, T)> { let mut b = a.drain(..= a.len() / 2).collect::<Vec<_>>(); b.reverse(); Self::refresh(a); Self::refresh(&mut b); b }
     fn merge(a: Option<T>, b: Option<T>) -> Option<T> { if let Some(x) = a { Some(if let Some(y) = b { x + y } else { x }) } else { b } }
     fn second(o: Option<&(T, T)>) -> Option<T> { o.map(|p| p.1.clone()) }
 }
